d200b45d7a190ad6745227b80b879f7cbe2ad108
AMILCAR OFICIAL <amilkarurquiagaramos1@gmail.com>
2026-06-30 17:15:46 -0500

    Actualizar creador-join.js

--- plugins/creador-join.js
+++ plugins/creador-join.js
@@ -93,7 +93,7 @@ const handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
 }
 
 handler.help    = ['join <link>']
-handler.tags    = ['owner']
+handler.tags    = ['creador']
 handler.command = ['join', 'unirse']
 handler.rowner  = true
 handler.desc    = 'Une el bot o sub-bot a un grupo'
